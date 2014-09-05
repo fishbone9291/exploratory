43959c6a978f80aae94ff012c2e1e6737f29d841
fishbone9291 <brian.kenneth.gallagher@gmail.com>
2014-09-05 12:25:45 -0400

    Changes done to script with Mike's help

--- R_all_sizes.R
+++ R_all_sizes.R
@@ -1,15 +1,14 @@
-# Set work directory
-setwd("C:/Users/brian/Desktop/Masters Thesis/Data Files/")
+# Work directory set by the connection to Github, must post more detailed
+# location information when importing data below
 
 # List relevant packages, so their commands are recognized
 library("dplyr")
 library("ggplot2")
-library("date")
+library("lubridate")
 library("reshape2")
 
 # Import data files, print and summarize
-all_sizes <- read.csv("all_sizes.csv", header=TRUE, sep=",")
-all_sizes
+all_sizes <- read.csv("C:/Users/brian/Desktop/Masters Thesis/Data Files/all_sizes.csv", header=TRUE, sep=",")
 summary(all_sizes)
 
 # Change data types and summarize
@@ -33,15 +32,25 @@ plot(weight~length,data=all_sizes)
 plot(length~rm,data=all_sizes)
 plot(logweight~loglength,data=all_sizes)
 
-# Split data by a factor, then re-make plots and histograms
-split_sizes<-split(all_sizes,all_sizes$age.class)
-yoy<-split_sizes[[1]]
-yrl<-split_sizes[[2]]
-adult<-split_sizes[[3]]
+# Split data by age class
+# Data are in [row,column] form. Blank space after comma implies that
+# you want nothing done to columns
+yoy <- all_sizes[all_sizes$age.class == 0,]
+yrl <- all_sizes[all_sizes$age.class == 1,]
+adult <- all_sizes[all_sizes$age.class == 2,]
 
-# Remake some histograms with split data
-hist(yoy$length,main="YOY Length Frequency",xlab="Length",breaks=seq(40,100,5))
-hist(yrl$length,main="Yearling Length Frequency",xlab="Length",breaks=seq(90,120,5))
+# Can also create a function and call it to separate data, same as above
+age.split <- function(class){
+  all_sizes[all_sizes$age.class == class,]
+}
+
+yoy <- age.split(0)
+yrl <- age.split(1)
+adult <- age.split(2)
+
+# Remake some histograms with split data by hitting them with a hammer
+hist(age.split(0)$length,main="YOY Length Frequency",xlab="Length",breaks=seq(40,100,5))
+hist(age.split(1)$length,main="Yearling Length Frequency",xlab="Length",breaks=seq(90,120,5))
 hist(adult$length,main="Adult Length Frequency",xlab="Length",breaks=seq(120,300,10))
 
 hist(yoy$weight,main="YOY Weight Frequency",xlab="Weight",breaks=seq(0,10,1))
@@ -52,7 +61,7 @@ hist(yoy$rm,main="YOY River Mile Frequency",xlab="River Mile",breaks=seq(0,150,1
 hist(yrl$rm,main="Yearling River Mile Frequency",xlab="River Mile",breaks=seq(0,150,10))
 hist(adult$rm,main="Adult River Mile Frequency",xlab="River Mile",breaks=seq(0,150,10))
 
-# Remake some plots with split data
+# Remake some plots with split data by hitting them with a hammer
 plot(weight~length,data=yoy,main="YOY Weight vs Length")
 plot(weight~length,data=yrl,main="Yearling Weight vs Length")
 plot(weight~length,data=adult,main="Adult Weight vs Length")
@@ -64,3 +73,7 @@ plot(length~rm,data=adult,main="Adt Length vs River Mile")
 plot(logweight~loglength,data=yoy,main="YOY loglog")
 plot(logweight~loglength,data=yrl,main="Yrl loglog")
 plot(logweight~loglength,data=adult,main="Adt loglog")
+
+# Or, make multiple summarizing plots simultaneously using ggplot
+# Holy crap!
+ggplot(all_sizes) + geom_histogram(aes(x=length), binwidth = 10) + facet_wrap(~age.class + site)
--- R_yoy_transects_13.R
+++ R_yoy_transects_13.R
@@ -1,18 +1,20 @@
-# Set work directory
-setwd("C:/Users/brian/Desktop/Masters Thesis/Data Files/")
+# Work directory set by the connection to Github, must post more detailed
+# location information when importing data below
 
 # List relevant packages, so their commands are recognized
 library("dplyr")
 library("ggplot2")
-library("date")
+library("lubridate")
+library("reshape2")
 library("methods")
 
 # Import data files, print and summarize
-transects <- read.csv("yoy_transects_13.csv", header=TRUE, sep=",")
+transects <- read.csv("C:/Users/brian/Desktop/Masters Thesis/Data Files/yoy_transects_13.csv", header=TRUE, sep=",")
 summary(transects)
 
 # Change data types and summarize
 transects$id <- as.factor(transects$id)
+transects$point <- as.factor(transects$point)
 summary(transects)
 
 # Make a new variable for Sr:ca
